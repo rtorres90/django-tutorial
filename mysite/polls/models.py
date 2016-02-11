003211b0731126edb610907d802cdf3e2635dc7c
rtorres90 <roberto1990z>
2016-02-11 11:11:14 -0300

    part 5.. completed!

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+import datetime
 from django.db import models
 from django.utils import timezone
 
@@ -8,7 +9,8 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def __unicode__(self):
         return "%s[%s]" % (self.question_text, self.id)
